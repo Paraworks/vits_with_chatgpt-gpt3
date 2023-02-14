162dd6b411c67faad3361ea341ec00c5052d1f03
Bateauxplanze <82026272+Paraworks@users.noreply.github.com>
2023-02-14 14:46:04 +0800

    Update api_launch.py

--- api_launch.py
+++ api_launch.py
@@ -23,7 +23,7 @@ def get_args():
     parser.add_argument('--cfg', default="./moe/config.json")
     parser.add_argument('--outdir', default="./moe",
                         help='ouput directory')
-    parser.add_argument('--key',default = "你的openai key",
+    parser.add_argument('--key',default = "your openai key",
                         help='openai api key')
     args = parser.parse_args()
     return args
